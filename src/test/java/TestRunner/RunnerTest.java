97dfeeb8dd10c24e20dc569c3da35687a790ce54
USER <USER@Nikita>
2023-08-11 11:08:48 +0530

    create new framework

---
+++ src/test/java/TestRunner/RunnerTest.java
@@ -0,0 +1,17 @@
+package TestRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		features= {"AllFeatureFiles"},
+		glue={"StepDefinition"},
+		dryRun=false,
+		tags="@LoginPage or ContactPage",
+		plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+		)
+public class RunnerTest extends AbstractTestNGCucumberTests{
+
+}
